4f930a36b8fb902a9e47b499df4f42e83d217a63
bossanova808 <bossanova808@gmail.com>
2021-04-27 16:54:16 +1000

    A bunch more BOM API work...

--- resources/lib/bom_forecast.py
+++ resources/lib/bom_forecast.py
@@ -38,9 +38,191 @@ Current.WindGust
 
 """
 
+WEATHER_CODES = {'clearing_shower': '39',
+                 'cloudy': '26',
+                 'cloud_and_wind_increasing': '23',
+                 'cloud_increasing': '26',
+                 'drizzle': '11',
+                 'drizzle_clearing': '39',
+                 'fog': '20',
+                 'fog_then_sunny': '34',
+                 'frost_then_sunny': '34',
+                 'hazy': '21',
+                 'heavy_rain': '40',
+                 'heavy_showers': '12',
+                 'increasing_sunshine': '30',
+                 'late_shower': '45',
+                 'late_thunder': '47',
+                 'mostly_cloudy': '26',
+                 'mostly_sunny': '34',
+                 'overcast': '26',
+                 'possible_shower': '11',
+                 'possible_thunderstorm': '37',
+                 'rain': '40',
+                 'rain_and_snow': '5',
+                 'rain_clearing': '39',
+                 'rain_developing': '12',
+                 'rain_tending to_snow': '5',
+                 'shower': '11',
+                 'showers': '11',
+                 'showers_easing': '11',
+                 'showers_increasing': '11',
+                 'snow': '41',
+                 'snowfalls_clearing': '5',
+                 'snow_developing': '13',
+                 'snow_showers': '41',
+                 'snow_tending to_rain': '5',
+                 'sunny': '32',
+                 'thunderstorms': '38',
+                 'thunderstorms': '38',
+                 'thunderstorms_clearing': '37',
+                 'windy': '23',
+                 'windy_with_rain': '2',
+                 'windy_with_showers': '2',
+                 'windy_with_snow': '43',
+                 'wind_and_rain_increasing': '2',
+                 'wind_and_showers_easing': '11',
+                 'unknown': 'na',
+                 'nt_unknown': 'na'}
 
-def utc_str_to_local_str(utc_str: str, utc_format: str = '%Y-%m-%dT%H:%M:%SZ', local_format: str = '%I:%M %p'):
+WEATHER_CODES_NIGHT = {'clearing_shower': '45',
+                       'cloudy': '29',
+                       'cloud_and_wind_increasing': '27',
+                       'cloud_increasing': '27',
+                       'drizzle': '45',
+                       'drizzle_clearing': '45',
+                       'fog': '20',
+                       'fog_then_sunny': '33',
+                       'frost_then_sunny': '33',
+                       'hazy': '33',
+                       'heavy_rain': '47',
+                       'heavy_showers': '45',
+                       'increasing_sunshine': '31',
+                       'late_shower': '45',
+                       'late_thunder': '47',
+                       'mostly_cloudy': '27',
+                       'mostly_sunny': '31',
+                       'overcast': '29',
+                       'possible_shower': '45',
+                       'possible_thunderstorm': '47',
+                       'rain': '45',
+                       'rain_and_snow': '46',
+                       'rain_clearing': '45',
+                       'rain_developing': '45',
+                       'rain_tending to_snow': '45',
+                       'shower': '45',
+                       'showers': '45',
+                       'showers_easing': '45',
+                       'showers_increasing': '45',
+                       'snow': '46',
+                       'snowfalls_clearing': '46',
+                       'snow_developing': '46',
+                       'snow_showers': '46',
+                       'snow_tending to_rain': '46',
+                       'sunny': '31',
+                       'thunderstorms': '47',
+                       'thunder-storms': '47',
+                       'thunderstorms_clearing': '47',
+                       'windy': '29',
+                       'windy_with_rain': '45',
+                       'windy_with_showers': '45',
+                       'windy_with_snow': '46',
+                       'wind_and_rain_increasing': '45',
+                       'wind_and_showers_easing': '45',
+                       'unknown': 'na',
+                       'nt_unknown': 'na'}
+
+"""
+These are the weather codes for XBMC is seems
+N/A Not Available
+0 Rain/Lightning
+01 Windy/Rain
+02 Same as 01
+03 Same as 00
+04 Same as 00
+05 Cloudy/Snow-Rain Mix
+06 Hail
+07 Icy/Clouds Rain-Snow
+08 Icy/Haze Rain
+09 Haze/Rain
+10 Icy/Rain
+11 Light Rain
+12 Moderate Rain
+13 Cloudy/Flurries
+14 Same as 13
+15 Flurries
+16 Same as 13
+17 Same as 00
+18 Same as 00
+19 Dust
+20 Fog
+21 Haze
+22 Smoke
+23 Windy
+24 Same as 23
+25 Frigid
+26 Mostly Cloudy
+27 Mostly Cloudy/Night
+28 Mostly Cloudy/Sunny
+29 Partly Cloudy/Night
+30 Partly Cloudy/Day
+31 Clear/Night
+32 Clear/Day
+33 Hazy/Night
+34 Hazy/Day
+35 Same as 00
+36 Hot!
+37 Lightning/Day
+38 Lightning
+39 Rain/Day
+40 Rain
+41 Snow
+42 Same as 41
+43 Windy/Snow
+44 Same as 30
+45 Rain/Night
+46 Snow/Night
+47 Thunder Showers/Night
+
+NIGHT SUBSET:
+27 Mostly Cloudy/Night
+29 Partly Cloudy/Night
+31 Clear/Night
+33 Hazy/Night
+45 Rain/Night
+46 Snow/Night
+47 Thunder Showers/Night
+"""
+
+def set_key(weather_data, index, key, value):
     """
+    Set a key - for old and new weather label support
+    """
+
+    value = str(value)
+
+    if index == 0:
+        weather_data['Current.' + key] = value.strip()
+        weather_data['Current.' + key] = value.strip()
+
+    weather_data['Day' + str(index) + '.' + key] = value.strip()
+    weather_data['Day' + str(index) + '.' + key] = value.strip()
+    weather_data['Daily.' + str(index + 1) + '.' + key] = value.strip()
+    weather_data['Daily.' + str(index + 1) + '.' + key] = value.strip()
+
+
+def set_keys(weather_data, index, keys, value):
+    """
+    Set a group of keys at once - for old and new weather label support
+    """
+    for key in keys:
+        set_key(weather_data, index, key, value)
+
+
+def utc_str_to_local_datetime(utc_str: str, utc_format: str = '%Y-%m-%dT%H:%M:%SZ'):
+    """
+    Given a UTC string, return a datetime in the local timezone
+
     :param utc_str: UTC time string
     :param utc_format: format of UTC time string
     :param local_format: format of local time string
@@ -48,7 +230,18 @@ def utc_str_to_local_str(utc_str: str, utc_format: str = '%Y-%m-%dT%H:%M:%SZ', l
     """
     temp1 = datetime.datetime.strptime(utc_str, utc_format)
     temp2 = temp1.replace(tzinfo=datetime.timezone.utc)
-    local_time = temp2.astimezone()
+    return temp2.astimezone()
+
+def utc_str_to_local_str(utc_str: str, utc_format: str = '%Y-%m-%dT%H:%M:%SZ', local_format: str = '%I:%M %p'):
+    """
+    Given a UTC string, return a string with the local time in the given format
+
+    :param utc_str: UTC time string
+    :param utc_format: format of UTC time string
+    :param local_format: format of local time string
+    :return: local time string
+    """
+    local_time = utc_str_to_local_datetime(utc_str, utc_format)
     return local_time.strftime(local_format).lstrip('0')
 
 
@@ -56,6 +249,9 @@ def utc_str_to_local_str(utc_str: str, utc_format: str = '%Y-%m-%dT%H:%M:%SZ', l
 # Fire danger rating may also be 'null'
 
 def fire_danger_to_text(fire_danger_float):
+
+    log(fire_danger_float)
+
     if fire_danger_float == 'null':
         return "None"
     elif 0.0 <= fire_danger_float <= 5.99:
@@ -156,37 +352,121 @@ def bom_forecast(geohash):
 
     # Gather the weather data into Kodi friendly labels
     weather_data = {}
-    weather_data['Current.Condition'] = forecast_seven_days[0]['short_text']
-    weather_data['Current.ConditionLong'] = forecast_seven_days[0]['extended_text']
-    weather_data['Current.Sunrise'] = weather_data['Today.Sunrise'] = utc_str_to_local_str(forecast_seven_days[0]['astronomical']['sunrise_time'])
-    weather_data['Current.Sunset'] = weather_data['Today.Sunset'] = utc_str_to_local_str(forecast_seven_days[0]['astronomical']['sunset_time'])
-    weather_data['Current.Temperature'] = str(round(current_observations['temp']))
-    weather_data['Current.FeelsLike'] = str(round(current_observations['temp_feels_like']))
-    weather_data['Current.Humidity'] = current_observations['humidity']
-    weather_data['Current.WindSpeed'] = current_observations['wind']['speed_kilometre']
-    weather_data['Current.WindDirection'] = current_observations['wind']['direction']
-    weather_data['Current.Wind'] = f'From {current_observations["wind"]["direction"]} at {current_observations["wind"]["speed_kilometre"]} km/h'
-    weather_data['Current.WindGust'] = f'{current_observations["gust"]["speed_kilometre"]} km/h'
-    weather_data['Current.FireDanger'] = '0' if forecast_seven_days[0]['fire_danger'] == 'null' else forecast_seven_days[0]['fire_danger']
-    weather_data['Current.FireDangerText'] = fire_danger_to_text(forecast_seven_days[0]['fire_danger'])
-    weather_data["Current.Precipitation"] = weather_data["Current.RainSince9"] = current_observations['rain_since_9am']
+
+    if current_observations:
+        weather_data['Current.Temperature'] = str(round(current_observations['temp']))
+        weather_data['Current.FeelsLike'] = str(round(current_observations['temp_feels_like']))
+        weather_data['Current.Humidity'] = current_observations['humidity']
+        weather_data['Current.WindSpeed'] = current_observations['wind']['speed_kilometre']
+        weather_data['Current.WindDirection'] = current_observations['wind']['direction']
+        weather_data['Current.Wind'] = f'From {current_observations["wind"]["direction"]} at {current_observations["wind"]["speed_kilometre"]} km/h'
+        weather_data['Current.WindGust'] = f'{current_observations["gust"]["speed_kilometre"]} km/h'
+        weather_data["Current.Precipitation"] = weather_data["Current.RainSince9"] = current_observations['rain_since_9am']
+
+    if forecast_seven_days:
+        weather_data['Current.Condition'] = forecast_seven_days[0]['short_text']
+        weather_data['Current.ConditionLong'] = forecast_seven_days[0]['extended_text']
+        weather_data['Current.Sunrise'] = weather_data['Today.Sunrise'] = utc_str_to_local_str(forecast_seven_days[0]['astronomical']['sunrise_time'])
+        weather_data['Current.Sunset'] = weather_data['Today.Sunset'] = utc_str_to_local_str(forecast_seven_days[0]['astronomical']['sunset_time'])
+        weather_data['Current.FireDanger'] = 'None' if not forecast_seven_days[0]['fire_danger'] else forecast_seven_days[0]['fire_danger']
+        weather_data["Current.NowLabel"] = forecast_seven_days[0]['now']['now_label']
+        weather_data["Current.LaterLabel"] = forecast_seven_days[0]['now']['later_label']
+
+        # For each day of the forecast...
+        for i, forecast_day in enumerate(forecast_seven_days):
+            forecast_datetime = utc_str_to_local_datetime(forecast_seven_days[i]['date'])
+            # The names for days - short (Mon) and long (Monday)
+            set_key(weather_data, i, "ShortDay", forecast_datetime.strftime('%a'))
+            set_key(weather_data, i, "Title", forecast_datetime.strftime('%A'))
+            set_key(weather_data, i, "LongDay", forecast_datetime.strftime('%A'))
+            # Date (Apr 4)
+            set_key(weather_data, i, "ShortDate", forecast_datetime.strftime('%b ') + forecast_datetime.strftime('%d').lstrip('0'))
+            # Outlook / Condition (same thing)
+            set_key(weather_data, i, "Outlook", forecast_seven_days[i]['short_text'])
+            set_key(weather_data, i, "Condition", forecast_seven_days[i]['short_text'])
+            set_key(weather_data, i, "OutlookLong", forecast_seven_days[i]['extended_text'])
+            set_key(weather_data, i, "ConditionLong", forecast_seven_days[i]['extended_text'])
+            # Weather icon (current day is different - we use night icons if it is night...)
+            icon_code = "na"
+            try:
+                if i == 0 and forecast_seven_days[i]['now']['is_night']:
+                    icon_code  = WEATHER_CODES_NIGHT[forecast_seven_days[i]['icon_descriptor']]
+                else:
+                    icon_code = WEATHER_CODES[forecast_seven_days[i]['icon_descriptor']]
+            except KeyError:
+                log(f'Could not find icon code for BOM icon_descriptor: "{forecast_seven_days[i]["icon_descriptor"]}"')
+
+            set_keys(weather_data, i, ["OutlookIcon", "ConditionIcon"], f'{icon_code}.png')
+            set_keys(weather_data, i, ["FanartCode"], icon_code)
+            # Maxes, Mins
+            set_keys(weather_data, i, ["HighTemp", "HighTemperature"], forecast_seven_days[i]['temp_max'])
+            set_keys(weather_data, i, ["LowTemp", "LowTemperature"], forecast_seven_days[i]['temp_min'])
+            # Chance & amount of rain
+            set_keys(weather_data, i, ["RainChance", "ChancePrecipitation"], forecast_seven_days[i]['rain']['chance'])
+            amount = forecast_seven_days[i]['rain']['amount']['max'] or '0'
+            set_keys(weather_data, i, ["RainChanceAmount", "Precipitation"], amount)
+            # UV - Predicted max, text for such, and the recommended 'Wear Sun Protection' period
+            set_key(weather_data, i, 'UVMaxIndex',  forecast_seven_days[i]['uv']['max_index'])
+            if forecast_seven_days[i]['uv']['category']:
+                set_key(weather_data, i, 'UVCategory', forecast_seven_days[i]['uv']['category'].title())
+            else:
+                set_key(weather_data, i, 'UVCategory', "None")
+            if forecast_seven_days[i]['uv']['start_time']:
+                set_key(weather_data, i, 'UVStartProtection', utc_str_to_local_str(forecast_seven_days[i]['uv']['start_time']))
+            else:
+                set_key(weather_data, i, 'UVStartProtection', 'N/A')
+            if forecast_seven_days[i]['uv']['end_time']:
+                set_key(weather_data, i, 'UVEndProtection', utc_str_to_local_str(forecast_seven_days[i]['uv']['end_time']))
+            else:
+                set_key(weather_data, i, 'UVEndProtection', 'N/A')
+
+        # Warnings -
+        warnings_text = ""
+
+        if warnings:
+            for i, warning in enumerate(warnings):
+                warning_issued = utc_str_to_local_str(warning['issue_time'])
+                warning_expires = utc_str_to_local_str(warning['expiry_time'])
+                warning_text = f'{warning["title"]} (issued at {warning_issued}, expires {warning_expires})'
+                warnings_text += warning_text
+                if i != len(warnings):
+                     warnings_text += '\n\n'
+
+        weather_data['Current.WarningsText'] = warnings_text
+
+        # Cleanup & Data massaging
+
+        # Forecast min can be None (presumably as 'past the forecast'), so instead then use the 'temp_later' (i.e. minumum to come)
+        if weather_data['Current.LowTemp'] == "None":
+            set_keys(weather_data, 0, ["LowTemp", "LowTemperature"], forecast_seven_days[0]['now']['temp_later'])
+
+
 
     # Missing data
     # weather_data['Current.DewPoint']
     # weather_data['Current.Pressure']
+    # weather_data['Current.FireDangerText'] -> use only FireDanger as is now text already
+    # TO-DOs
+    # 3 hourly forecast?
+    # Rain 3 hour forecast?
+    
+    return weather_data
 
-    log(weather_data)
 
 ###########################################################
 # MAIN (only for unit testing outside of Kodi)
 
 if __name__ == "__main__":
 
-    geohashes_to_test = ['r1r11df', 'r1f94ew']
+    geohashes_to_test = ['r1r11df'] # ['r1r11df', 'r1f94ew']
     for geohash in geohashes_to_test:
         log(f'Getting weather data from BOM for geohash "{geohash}"')
-        bom_forecast(geohash)
+        weather_data = bom_forecast(geohash)
 
+        for key in sorted(weather_data):
+            if weather_data[key] == "?" or weather_data[key] == "na":
+                log("**** MISSING: ")
+            log(f'{key}: "{weather_data[key]}"')
 
 """
 BOM API
